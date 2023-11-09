130ba1710a9c1d8365f52e12512a29807691a0cd
mlvestlund <mariavestlund@yahoo.se>
2023-11-09 17:50:18 +0100

    Added admin and changed menu
    Added admin-functions from the 8/11-23-lesson and moved menu-functions to its own class and changed the menu to english

---
+++ MandelsBankenConsole/Utilities/DbHelper.cs
@@ -0,0 +1,34 @@
+﻿using MandelsBankenConsole.Data;
+using MandelsBankenConsole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelsBankenConsole.Utilities
+{
+    internal static class DbHelper
+    {
+        public static List<User> GetAllUsers(BankenContext context)
+        {
+            List<User> users = context.Users.ToList();
+            return users;
+        }
+
+        public static bool AddUser(BankenContext context, User user)
+        {
+            context.Users.Add(user);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error adding user: {e}");
+                return false;
+            }
+            return true;
+        }
+    }
+}
