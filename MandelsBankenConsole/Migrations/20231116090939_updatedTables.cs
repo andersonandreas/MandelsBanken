3aa9701bec097f6377c704681c7cd18acb938087
andersonandreas <adde1990@live.se>
2023-11-16 10:11:28 +0100

    fixed migration

---
+++ MandelsBankenConsole/Migrations/20231116090939_updatedTables.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MandelsBankenConsole.Migrations
+{
+    public partial class updatedTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- MandelsBankenConsole/Models/Transaction.cs
+++ MandelsBankenConsole/Models/Transaction.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public decimal TransactionAmount { get; set; }
         public decimal Balance { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime Date { get; set; }
 
         // FK
