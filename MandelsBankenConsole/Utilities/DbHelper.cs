6469b24c70022f8ef774fecc1ce655e761e7248e
Madagasikara <m66@o2.pl>
2023-11-21 14:21:55 +0100

    Better comments, changed inparameter of GetAccountInformation
    Improved comments in BankTransfer, ShowAccounts and in some methods in DbHelper. GetAccountInformation got an List<string> as inparameter instead of an array. MenuFunctions and ShowMenu have been updated after that.

--- MandelsBankenConsole/Utilities/DbHelper.cs
+++ MandelsBankenConsole/Utilities/DbHelper.cs
@@ -8,7 +8,8 @@ namespace MandelsBankenConsole.Utilities
     {
         public static User GetUserByLogInInput(BankenContext context, string userLogInInput)
         {
-            return context.Users.SingleOrDefault(u => u.SocialSecurityNumber == userLogInInput);
+            return context.Users
+                .SingleOrDefault(u => u.SocialSecurityNumber == userLogInInput);
         }
 
         public static List<User> GetAllUsers(BankenContext context)
@@ -36,29 +37,51 @@ namespace MandelsBankenConsole.Utilities
         {
 
             return context.Accounts
-               .Include(acc => acc.Currency) // ChatGPT helped with adding Currency here cause of nullable fields
+               .Include(acc => acc.Currency) 
                .Where(acc => acc.UserId == user.Id)
                .ToList();
             
         }
 
-        public static List<string> GetAccountInformation(List<Account> accounts, bool balance = true)
+        public static List<string> GetAccountInformation(List<Account> accounts, bool balanceOverOwner = true)
         {
-            if (balance) { 
+            // Creates a list of string with descriptive information on the chosen accounts
+
+            if (balanceOverOwner) { 
             return accounts
-                .Select(a => $"Account: {a.AccountNumber} {a.AccountName}\tBalance: {a.Balance} {a.Currency.CurrencyCode}")
+                .Select(a => $"Account: {a.AccountNumber} - {a.AccountName} | Balance: {a.Balance:# ##0.##} {a.Currency.CurrencyCode}")
                 .ToList();
             }
 
             return accounts
-                .Select(a => $"Account: {a.AccountNumber} {a.AccountName}\tCurrency: {a.Currency.CurrencyCode}")
+                .Select(a => $"Account: {a.AccountNumber} - {a.AccountName} | Owner: {a.User.CustomerName} | Currency: {a.Currency.CurrencyCode}")
                 .ToList();
         }
+        public static bool UpdateBalance(BankenContext context, Account account, decimal amount)
+        {
 
+            // Updates balance of an account after a transaction
+            // amount is negative if money is drawn from the account
+
+            account.Balance = Math.Round(account.Balance + amount, 2);
+
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error updating balance: {e}");
+                return false;
+            }
+
+            return true;
+
+        }
         public static bool AddTransaction(BankenContext context, Account account, decimal amount, string transactionInfo)
         {
 
-            // adds the transaction after the balance was updated
+            // Adds transaction to the Db after account's balance was updated
 
             Transaction newTransaction = new Transaction()
             {
@@ -81,34 +104,14 @@ namespace MandelsBankenConsole.Utilities
             }
             return true;
         }
-
-        public static bool UpdateBalance(BankenContext context, Account account, decimal amount)
-        {
-
-            // updates balance of an account
-
-            account.Balance += amount;
-
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error updating balance: {e}");
-                return false;
-            }
-
-            return true;
-
-        }
         public static bool MakeTransaction(BankenContext context, Account account, decimal amount, string transactionInfo)
         {
-            // validate if user has enough money for the transfer, update the balance, add a transaction
+            // Validate if user has enough money for the transaction, updates account's balance, adds a transaction
 
             if (account.Balance + amount < 0)
             {
-                Console.WriteLine($"Not enough money :( your balance: {account.Balance} {account.Currency.CurrencyCode}, you try to transfer {-amount}");
+                Console.WriteLine($"You don't have enough funds in your account to perform this transaction.");
+                Console.WriteLine($"Your balance is {account.Balance} {account.Currency.CurrencyCode}, you try to use -{amount} {account.Currency.CurrencyCode}.");
                 return false;
             }
 
@@ -123,101 +126,6 @@ namespace MandelsBankenConsole.Utilities
             }
 
             return true;
-        }
-
-        public static List<Account> GetAllAccounts(BankenContext context, User user)
-        {
-
-            return context.Accounts
-               .Include(acc => acc.Currency) // ChatGPT helped with adding Currency here cause of nullable fields
-               .Where(acc => acc.UserId == user.Id)
-               .ToList();
-
-        }
-
-        public static List<string> GetAccountInformation(List<Account> accounts, bool balance = true)
-        {
-            if (balance)
-            {
-                return accounts
-                    .Select(a => $"Account: {a.AccountNumber} {a.AccountName}\tBalance: {a.Balance} {a.Currency.CurrencyCode}")
-                    .ToList();
-            }
-
-            return accounts
-                .Select(a => $"Account: {a.AccountNumber} {a.AccountName}\tCurrency: {a.Currency.CurrencyCode}")
-                .ToList();
-        }
-
-        public static bool AddTransaction(BankenContext context, Account account, decimal amount, string transactionInfo)
-        {
-
-            // adds the transaction after the balance was updated
-
-            Transaction newTransaction = new Transaction()
-            {
-                TransactionAmount = amount,
-                Balance = account.Balance,
-                Description = transactionInfo,
-                Date = DateTime.Now,
-                AccountId = account.Id,
-
-            };
-            context.Transactions.Add(newTransaction);
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error adding transaction: {e}");
-                return false;
-            }
-            return true;
-        }
-
-        public static bool UpdateBalance(BankenContext context, Account account, decimal amount)
-        {
-
-            // updates balance of an account
-
-            account.Balance += amount;
-
-            try
-            {
-                context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error updating balance: {e}");
-                return false;
-            }
-
-            return true;
-
-        }
-        public static bool MakeTransaction(BankenContext context, Account account, decimal amount, string transactionInfo)
-        {
-            // validate if user has enough money for the transfer, update the balance, add a transaction
-
-            if (account.Balance + amount < 0)
-            {
-                Console.WriteLine($"Not enough money :( your balance: {account.Balance} {account.Currency.CurrencyCode}, you try to transfer {-amount}");
-                return false;
-            }
-
-            if (!UpdateBalance(context, account, amount))
-            {
-                return false;
-            }
-
-            if (!AddTransaction(context, account, amount, transactionInfo))
-            {
-                return false;
-            }
-
-            return true;
-
         }
 
     }
